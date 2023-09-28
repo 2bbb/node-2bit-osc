ed396b3adbb4b7ee2f0d14c8afe09f86dd8f138c
2bit <i@2bit.jp>
2023-09-28 11:52:37 +0900

    forget to add

--- src/module/types/argument.ts
+++ src/module/types/argument.ts
@@ -7,3 +7,15 @@ export type SpecifiedArgument<T extends TypeName> = {
 
 export type Argument = SpecifiedArgument<TypeName>;
 export type ArgumentLike = Argument | TypeNameToValueType[TypeName] | BufferLike;
+
+export function int32(v: number | bigint): SpecifiedArgument<'integer'> {
+    return { type: 'integer', value: Number(v) };
+}
+
+export function float(v: number | bigint): SpecifiedArgument<'float'> {
+    return { type: 'float', value: Number(v) };
+}
+
+export function double(v: number | bigint): SpecifiedArgument<'double'> {
+    return { type: 'double', value: Number(v) };
+}
